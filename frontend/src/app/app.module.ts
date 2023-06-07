71e5ade3aef06396813244285593272f20edeeeb
Marcel Kaiser <marcel.philipp.kaiser@mni.thm.de>
2023-06-07 11:39:52 +0200

    add html page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth/auth.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { AuthModule } from './auth/auth/auth.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule,
     AuthModule
   ],
   providers: [],
