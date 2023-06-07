ae53cc049489c7c8a07eb861443b06db23bfc5e0
Marcel Kaiser <marcel.philipp.kaiser@mni.thm.de>
2023-06-07 15:02:16 +0200

    Frontend verfügbar

--- frontend/src/app/auth/auth/auth.module.ts
+++ frontend/src/app/auth/auth/auth.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
@@ -11,7 +13,10 @@ import { RegisterComponent } from './components/register/register.component';
     RegisterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
   ],
   exports : [
     LoginComponent,
