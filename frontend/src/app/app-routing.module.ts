c52b3f82a7b795d280a6b0ca748e1fa6d08650d8
Marcel Kaiser <marcel.philipp.kaiser@mni.thm.de>
2023-06-07 11:21:31 +0200

    Added Auth Module

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/auth/components/login/login.component';
+import { RegisterComponent } from './auth/auth/components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: 'login', component: LoginComponent},
+{path: 'register', component: RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ frontend/src/app/auth/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports : [
+    LoginComponent,
+    RegisterComponent
+  ]
+})
+export class AuthModule { }
