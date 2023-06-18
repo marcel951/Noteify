efad65a738033e5ccdebd4aee5e5db27436e72e2
Marcel Kaiser <marcel.philipp.kaiser@mni.thm.de>
2023-06-18 16:03:15 +0200

    build search component

--- frontend/src/app/main/search/search.component.ts
+++ frontend/src/app/main/search/search.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  searchTerm: string = '';
+  searchResults: any[]= []; // Ergebnisse der API-Suche
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private apiService: ApiService
+  ) {}
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.searchTerm = params['q'] || '';
+      this.search();
+    });
+  }
+
+  search() {
+    this.apiService.getTypeRequest(`home/search?query=${this.searchTerm}`)
+      .subscribe((results: any) => {
+        this.searchResults = results;
+        console.log(this.searchResults);
+      });
+  }
+
+  navigateToSearch() {
+    if (this.searchTerm) {
+      this.router.navigate(['/search'], { queryParams: { q: this.searchTerm } });
+    }
+  }
 }
