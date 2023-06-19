18ddf3938c1f0cf05ade2b592b15c37cba51ce30
Jonathan Rech <joni.w.rech@gmail.com>
2023-06-19 14:29:54 +0200

    style: login + register + log-out button appears

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'frontend';
+  isLogin: boolean = false
+  constructor(
+    private _auth: AuthService,
+    private _router: Router
+  ){}
+  ngOnInit(){
+    this.isUserLogin();
+  }
+  isUserLogin(){
+    if(this._auth.getUserDetails() != null){this.isLogin = true;
+  }}
+  update(){
+    this.isUserLogin();
+  }
+  logout(){
+  this._auth.clearStorage()
+  this._router.navigate(['']);
+  this.isLogin = false;
+  }
 }
